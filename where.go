4a5da3152a8499f2bc72bf3c18125e603b86e336
lawrence lin <lawrence.lin@airbnb.com>
2019-05-04 21:56:08 +0800

    wechat auth

--- where.go
+++ where.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"net/http"
+	"sort"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hello world")
 }
 
+func AuthHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	token := "catlaw"
+	hashList := []string{
+		token,
+		r.Form.Get("timestamp"),
+		r.Form.Get("nonce"),
+	}
+	sort.Strings(hashList)
+	hashCode := sha1.Sum([]byte(hashList[0] + hashList[1] + hashList[2]))
+	hashString := string(hashCode[:])
+	fmt.Printf("%s, %s, %s", r.Form.Get("timestamp"), r.Form.Get("nounce"), hashString)
+	if hashString == r.Form.Get("signature") {
+		fmt.Fprint(w, r.Form.Get("echostr"))
+	} else {
+		fmt.Fprint(w, "")
+	}
+}
+
 func main() {
 	http.HandleFunc("/", IndexHandler)
-	http.ListenAndServe("127.0.0.1:80", nil)
+	http.HandleFunc("/wx", AuthHandler)
+	http.ListenAndServe(":8080", nil)
 }
