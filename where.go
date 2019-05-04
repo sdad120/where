f690ea8446c2ce2181200f3eab503e1f18ad2a1a
lawrence lin <lawrence.lin@airbnb.com>
2019-05-04 23:48:35 +0800

    support string to byte

--- where.go
+++ where.go
@@ -5,7 +5,21 @@ import (
 	"fmt"
 	"net/http"
 	"sort"
+	"strconv"
 )
+
+func hexStringToBytes(s string) [20]byte {
+	bs := make([]byte, 0)
+	for i := 0; i < len(s); i = i + 2 {
+		b, _ := strconv.ParseInt(s[i:i+2], 16, 16)
+		bs = append(bs, byte(b))
+	}
+
+	var fixBs [20]byte
+	copy(fixBs[:], bs[:20])
+
+	return fixBs
+}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "hello world")
@@ -21,9 +35,8 @@ func AuthHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	sort.Strings(hashList)
 	hashCode := sha1.Sum([]byte(hashList[0] + hashList[1] + hashList[2]))
-	hashString := string(hashCode[:])
-	fmt.Printf("%s, %s, %s", r.Form.Get("timestamp"), r.Form.Get("nounce"), hashString)
-	if hashString == r.Form.Get("signature") {
+	signatureCode := hexStringToBytes(r.Form.Get("signature"))
+	if hashCode == signatureCode {
 		fmt.Fprint(w, r.Form.Get("echostr"))
 	} else {
 		fmt.Fprint(w, "")
